e0c8b0de0e9003d93fc7b6ebc28866e1b6d2d627
Lindbrum <ultrascati95@gmail.com>
2025-02-08 11:38:39 +0100

    Partial implementation of energy analyzer; modified the start script to detach the docker containers from the terminal

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,7 +1,13 @@
+from enum import IntEnum
 import json
 import time
 
 import paho.mqtt.client as mqtt
+
+class Level(IntEnum):
+    NORMAL = 0
+    WARNING = 1
+    CRITICAL = 2
 
 class Analyzer:
     client_id = None
@@ -10,6 +16,7 @@ class Analyzer:
     topic_pub = "/smart/planner"
 
     def __init__(self, client_id, server, port):
+        # set up the mqtt client and other instance fields
         self.client_id = client_id
         self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id=client_id)
         self.server = server
@@ -17,8 +24,18 @@ class Analyzer:
         self.configuration = None
 
     def start(self):
+        # connect to the broker
         self.client.connect(self.server, self.port, 60)
         print("Connected to MQTT broker")
+
+        # Retrieve configuration
+        topic = self.config_topic + f"/{self.client_id}"
+        self.client.subscribe(topic)
+        print(f'({self.client_id}) Subscribing to topic: {topic}')
+
+        # subscribe to metrics
+        print(f'({self.client_id}) Subscribing to topic: {self.topic_sub}')
+        self.client.subscribe(self.topic_sub)
 
         #self.client.on_message = self.on_message
 
@@ -28,9 +45,12 @@ class Analyzer:
     def on_message(self, client, user_data, message):
         print(f'({self.client_id}) Received message: ', message.payload.decode('utf-8'))
 
+    def on_subscribe(self, client, userdata, mid, reason_code_list, properties):
+        print(f'({self.client_id}) Subscribed successfully')
+
     def parse_json_from_message(self, mqtt_message):
-        decoded = mqtt_message.payload.decode('utf-8')
-        parsed = json.loads(decoded)
+        decoded = mqtt_message.payload.decode('utf-8') #decode json string
+        parsed = json.loads(decoded) #parse json string into a dict
         return parsed
 
     def extract_values_from_message(self, mqtt_message):
@@ -40,30 +60,30 @@ class Analyzer:
         if 'configuration' in payload:
             return payload
         # extract the values (they are ordered from oldest to newest)
-        values = [item['_value'] for item in payload]
-        return values
+        values = {}
+        for name,metric in payload.items():
+            values[name] = [entry['_value'] for entry in metric]
+            return values
+        raise ValueError(f'Unable to extract values from message:\n{payload}')
 
 
 class TemperatureAnalyzer(Analyzer):
 
     def __init__(self, client_id="temperature_analyzer", server="localhost", port=1883):
-        super().__init__(client_id, server, port)
+        super().__init__(client_id, server, port) # setup mqtt client and instance fields
+
+        # set up callbacks and topic strings
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
         self.topic_sub = self.topic_sub + "/float/temperature"
         self.start()
 
     def start(self):
-        super().start()
-
-        # Retrieve configuration
-        self.client.subscribe(self.config_topic + "/temperature_analyzer")
-
-        self.client.subscribe(self.topic_sub)
-        print("Subscribed to topic " + self.topic_sub)
+        super().start() # connect to broker, retrieve config and subscribe to topics
 
         #initialize topics
         self.client.publish(self.topic_pub + "/increase_temperature", False, retain=True)
 
-        self.client.on_message = self.on_message
         self.loop()
 
     def loop(self):
@@ -72,54 +92,71 @@ class TemperatureAnalyzer(Analyzer):
     def on_message(self, client, user_data, message):
         super().on_message(client, user_data, message)
         values = self.extract_values_from_message(message)
+
         if 'configuration' in values:
             self.configuration = values['configuration']
             return
         if values: # check if new data is received (payload may be empty if sensors are not sending data)
-            if values[-1] <= self.configuration['min_temp']:
+            temperature = values['temperature']
+            if temperature[-1] <= self.configuration['min_temp']:
                 # tell planner to increase temperature
                 self.client.publish(self.topic_pub + "/increase_temperature", True, retain=True)
-            elif values[-1] >= self.configuration['min_temp'] + self.configuration['working_threshold']:
+            elif temperature[-1] >= self.configuration['min_temp'] + self.configuration['working_threshold']:
                 self.client.publish(self.topic_pub + "/increase_temperature", False, retain=True)
 
-class LightAnalyzer(Analyzer):
-    def __init__(self, client_id="light_analyzer", server="localhost", port=1883):
-        super().__init__(client_id, server, port)
-        self.topic_sub = self.topic_sub + "/int/light"
-        self.start()
+# class LightAnalyzer(Analyzer):
+#     def __init__(self, client_id="light_analyzer", server="localhost", port=1883):
+#         super().__init__(client_id, server, port) # setup mqtt client and instance fields
+#
+#         # set up callbacks and topic strings
+#         self.client.on_message = self.on_message
+#         self.client.on_subscribe = self.on_subscribe
+#         self.topic_sub = self.topic_sub + "/int/light"
+#         self.start()
+#
+#     def start(self):
+#         super().start() # connect to broker, retrieve config and subscribe to topics
+#
+#         # initialize topics
+#         # -1: decrease lighting, 0: mantain lighting, +1: increase lighting
+#         self.client.publish(self.topic_pub + "/change_lighting", 0, retain=True)
+#
+#         self.loop()
+#
+#     def loop(self):
+#         self.client.loop_start()
+#
+#     def on_message(self, client, user_data, message):
+#         super().on_message(client, user_data, message)
+#         values = self.extract_values_from_message(message)
+#
+#         if 'configuration' in values:
+#             self.configuration = values['configuration']
+#             return
+#
+#         if values:  # check if new data is received (payload may be empty if sensors are not sending data)
+#             return
 
-    def start(self):
-        super().start()
-        self.client.subscribe(self.topic_sub)
-        print("Subscribed to topic " + self.topic_sub)
-
-        self.client.on_message = self.on_message
-        self.loop()
-
-    def loop(self):
-        self.client.loop_start()
-
-    def on_message(self, client, user_data, message):
-        super().on_message(client, user_data, message)
-        values = self.extract_values_from_message(message)
-        if 'configuration' in values:
-            self.configuration = values['configuration']
-            return
-        if values:  # check if new data is received (payload may be empty if sensors are not sending data)
-            return
-            #Do something
 class EnergyAnalyzer(Analyzer):
+
     def __init__(self, client_id="energy_analyzer", server="localhost", port=1883):
         super().__init__(client_id, server, port)
+        # set up callbacks and topic strings
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
         self.topic_sub = self.topic_sub + "/int/energy"
+
+        # analyzer specific fields
+        self.energy_level = Level.NORMAL
+
         self.start()
 
     def start(self):
         super().start()
-        self.client.subscribe(self.topic_sub)
-        print("Subscribed to topic " + self.topic_sub)
 
-        self.client.on_message = self.on_message
+        # initialize topics
+        self.client.publish(self.topic_pub + "/energy_level", str(self.energy_level), retain=True) #0: OK, 1: high, 2: above limit
+
         self.loop()
 
     def loop(self):
@@ -132,69 +169,86 @@ class EnergyAnalyzer(Analyzer):
             self.configuration = values['configuration']
             return
         if values: # check if new data is received (payload may be empty if sensors are not sending data)
-            return
-            #Do something
-
-class FridgeLoadAnalyzer(Analyzer):
-    def __init__(self, client_id="fridge_load_analyzer", server="localhost", port=1883):
-        super().__init__(client_id, server, port)
-        self.topic_sub = self.topic_sub + "/int/fridge_load"
-        self.start()
-
-    def start(self):
-        super().start()
-        self.client.subscribe(self.topic_sub)
-        print("Subscribed to topic " + self.topic_sub)
-
-        self.client.on_message = self.on_message
-        self.loop()
-
-    def loop(self):
-        self.client.loop_start()
-
-    def on_message(self, client, user_data, message):
-        super().on_message(client, user_data, message)
-        values = self.extract_values_from_message(message)
-        if 'configuration' in values:
-            self.configuration = values['configuration']
-            return
-        if values: # check if new data is received (payload may be empty if sensors are not sending data)
-            return
-            #Do something
-
-class FridgeTempAnalyzer(Analyzer):
-    def __init__(self, client_id="fridge_temp_analyzer", server="localhost", port=1883):
-        super().__init__(client_id, server, port)
-        self.topic_sub = self.topic_sub + "/float/fridge_temp"
-        self.start()
-
-    def start(self):
-        super().start()
-        self.client.subscribe(self.topic_sub)
-        print("Subscribed to topic " + self.topic_sub)
-
-        self.client.on_message = self.on_message
-        self.loop()
-
-    def loop(self):
-        self.client.loop_start()
-
-    def on_message(self, client, user_data, message):
-        super().on_message(client, user_data, message)
-        values = self.extract_values_from_message(message)
-        if 'configuration' in values:
-            self.configuration = values['configuration']
-            return
-        if values: # check if new data is received (payload may be empty if sensors are not sending data)
-            return
-            #Do something
+            # check current total kw
+            total_kw = values['energy']
+            if total_kw[-1] <= self.configuration['warning_threshold_kw']:
+                if self.energy_level != Level.NORMAL:
+                    self.client.publish(self.topic_pub + "/energy_level", str(Level.NORMAL), retain=True)
+                    self.energy_level = Level.NORMAL
+            elif total_kw[-1] <= self.configuration['max_total_kw']:
+                if self.energy_level != Level.WARNING:
+                    self.client.publish(self.topic_pub + "/energy_level", str(Level.WARNING), retain=True)
+                    self.energy_level = Level.WARNING
+            else:
+                if self.energy_level != Level.CRITICAL:
+                    self.client.publish(self.topic_pub + "/energy_level", str(Level.CRITICAL), retain=True)
+                    self.energy_level = Level.CRITICAL
+# class FridgeLoadAnalyzer(Analyzer):
+#     def __init__(self, client_id="fridge_load_analyzer", server="localhost", port=1883):
+#         super().__init__(client_id, server, port)
+#         # set up callbacks and topic strings
+#         self.client.on_message = self.on_message
+#         self.client.on_subscribe = self.on_subscribe
+#         self.topic_sub = self.topic_sub + "/int/fridge_load"
+#         self.start()
+#
+#     def start(self):
+#         super().start()
+#         self.client.subscribe(self.topic_sub)
+#         print("Subscribed to topic " + self.topic_sub)
+#
+#         self.client.on_message = self.on_message
+#         self.loop()
+#
+#     def loop(self):
+#         self.client.loop_start()
+#
+#     def on_message(self, client, user_data, message):
+#         super().on_message(client, user_data, message)
+#         values = self.extract_values_from_message(message)
+#         if 'configuration' in values:
+#             self.configuration = values['configuration']
+#             return
+#         if values: # check if new data is received (payload may be empty if sensors are not sending data)
+#             return
+#             #Do something
+#
+# class FridgeTempAnalyzer(Analyzer):
+#     def __init__(self, client_id="fridge_temp_analyzer", server="localhost", port=1883):
+#         super().__init__(client_id, server, port)
+#         # set up callbacks and topic strings
+#         self.client.on_message = self.on_message
+#         self.client.on_subscribe = self.on_subscribe
+#         self.topic_sub = self.topic_sub + "/float/fridge_temp"
+#         self.start()
+#
+#     def start(self):
+#         super().start()
+#         self.client.subscribe(self.topic_sub)
+#         print("Subscribed to topic " + self.topic_sub)
+#
+#         self.client.on_message = self.on_message
+#         self.loop()
+#
+#     def loop(self):
+#         self.client.loop_start()
+#
+#     def on_message(self, client, user_data, message):
+#         super().on_message(client, user_data, message)
+#         values = self.extract_values_from_message(message)
+#         if 'configuration' in values:
+#             self.configuration = values['configuration']
+#             return
+#         if values: # check if new data is received (payload may be empty if sensors are not sending data)
+#             return
+#             #Do something
 
 def main():
     temp_analyzer = TemperatureAnalyzer()
-    light_analyzer = LightAnalyzer()
+    # light_analyzer = LightAnalyzer()
     energy_analyzer = EnergyAnalyzer()
-    fridge_load_analyzer = FridgeLoadAnalyzer()
-    fridge_temp_analyzer = FridgeTempAnalyzer()
+    # fridge_load_analyzer = FridgeLoadAnalyzer()
+    # fridge_temp_analyzer = FridgeTempAnalyzer()
 
     while True:
         time.sleep(0.1)
